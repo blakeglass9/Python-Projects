6a5c3d0d777f70492594d1637c3b234c89bb837c
blakeglass9 <146895621+blakeglass9@users.noreply.github.com>
2024-08-01 13:32:59 -0700

    ok

--- pythontutorial.py
+++ pythontutorial.py
@@ -1,105 +1,96 @@
-"""
 #
-#Python: 3.11.7
+# Python: 3.11.7
 #
-#Author: Blake Glass
+# Author: Blake Glass
 #
-#Purpose: The Tech Acadrmy - Python Course, Creating our first program together.
+# Purpose: The Tech Academy - Python Course, Creating our first program together.
 #
-"""
-def start(nice=0,mean=0,name="""):
-# get user's name
-name = describe_game(name)
-nice,mean,name = nice_mean(nice,mean,name)
+
+def start(nice=0, mean=0, name=""):
+    # Get the user's name
+    name = describe_game(name)
+    nice, mean, name = nice_mean(nice, mean, name)
 
 def describe_game(name):
-"""):
-check if this is a new game or not.
-If it is new, get the users name.
-If it is not a new game, thank the player for
-playing again and continue with the game
-""
-#meaning, if we do not already have this user's name,
-# then they are a new player and we need to get their name
-if name != "":
-print("\nThank you for playing again, ()!".format(name))
-else:
-stop = True
-while stop:
-if name == "":
-name = input("\nWhat is your name? \n>>> ").capitalize()
-if name != "":
-print("\nWelcome, ()!".format(name))
-print("\nIn this game, you will be greeted \nby several different people. \nYou can choose to be nice or mean")
-print("but at the end of the game your fate \nwill be sealed by your actions.")
-stop = False
-return name
+    """
+    Check if this is a new game or not.
+    If it is new, get the user's name.
+    If it is not a new game, thank the player for playing again and continue with the game.
+    """
+    if name != "":
+        print("\nThank you for playing again, {}!".format(name))
+    else:
+        stop = True
+        while stop:
+            if name == "":
+                name = input("\nWhat is your name? \n>>> ").capitalize()
+            if name != "":
+                print("\nWelcome, {}!".format(name))
+                print("\nIn this game, you will be greeted by several different people.")
+                print("You can choose to be nice or mean, but at the end of the game, your fate will be sealed by your actions.")
+                stop = False
+    return name
 
-
-def nice_mean(nice,mean,name):
+def nice_mean(nice, mean, name):
     stop = True
     while stop:
-        show_score(nice,mean,name)
-        pick = input("\nA stranger approaches you for a \nconversation. Will you be nice \or mean (N/M) \n>>>: ").lower()
+        show_score(nice, mean, name)
+        pick = input("\nA stranger approaches you for a conversation. Will you be nice or mean (N/M)? \n>>> ").lower()
         if pick == "n":
             print("\nThe stranger walks away smiling...")
-            nice = (nice + 1)
+            nice += 1
             stop = False
-        if pick == "m":
-            print ("\nThe stranger glares at you \nmenacingly and storms off...")
-            mean = (mean +1)
-            stop = false
-        score(nice,mean,name) # pass the 3 variables to the score()
+        elif pick == "m":
+            print("\nThe stranger glares at you menacingly and storms off...")
+            mean += 1
+            stop = False
+        else:
+            print("Invalid input. Please choose 'N' for nice or 'M' for mean.")
+    score(nice, mean, name)  # Pass the 3 variables to the score function.
 
+def show_score(nice, mean, name):
+    print("\n{}, your current total: \n({} Nice) and ({} Mean)".format(name, nice, mean))
 
+def score(nice, mean, name):
+    # Score function is being passed the values stored within the 3 variables
+    if nice > 2:  # If condition is valid, call win function passing in the variables so it can use them
+        win(nice, mean, name)
+    elif mean > 2:  # If condition is valid, call lose function passing in the variables so it can use them
+        lose(nice, mean, name)
+    else:  # Else, call nice_mean function passing in the variables so it can use them.
+        nice_mean(nice, mean, name)
 
-def show_score(nice,mean,name):
-    print ("\n(), your current total: \n((), Nice) and ((), Mean)".format(name,nice,mean))
+def win(nice, mean, name):
+    # Substitute the {} wildcards with our variable values
+    print("\nNice job, {}! You win! \nEveryone loves you and you've made lots of friends along the way!".format(name))
+    # Call again function and pass in our variables
+    again(nice, mean, name)
 
+def lose(nice, mean, name):
+    # Substitute the {} wildcards with our variable values
+    print("\nOh no, {}. You lose. You've been too mean, and everyone is upset with you.".format(name))
+    # Call again function and pass in our variables
+    again(nice, mean, name)
 
-def score(nice,mean,name):
-    # score function is being passed the values stored within the 3 variables
-    if nice > 2: # if condition is valid, call lose function passing in the variables so it can use them
-        win(nice,mean,name)
-    if mean > 2: # if condition is valid, call lose function passing in the variable so it can use them
-        lose(nice,mean,name)
-    else:   #else, call nice_mean function passing in the variable so it can use them.
-        nice_mean(nice,mean,name)
-
-
-def win(nice,mean,name):
-    #Substitute the () wildcards with out variable values
-    print("\nNice job (), you win! \nEveryone loves you and you've \nmade lots of friends along the way!".format(name))
-    # call again function and pass in our variables
-    again(nice,mean,name)
-
-def again(nice,mean,name):
+def again(nice, mean, name):
     stop = True
     while stop:
         choice = input("\nDo you want to play again? (y/n):\n>>> ").lower()
         if choice == "y":
             stop = False
-            reset(nice,mean,name)
-        if choice == "n":
+            reset(nice, mean, name)
+        elif choice == "n":
             print("\nOh, so sad, sorry to see you go!")
             stop = False
             quit()
         else:
-            print("\nEnter ( Y ) for 'YES'. ( N) for 'NO':\n>>> ")
+            print("\nEnter 'y' for 'YES' or 'n' for 'NO':\n>>> ")
 
-def reset(nice,mean,name):
+def reset(nice, mean, name):
     nice = 0
     mean = 0
-    #Notice, I do not reset the same variable as that same user has elected to play again
-    start(nice,mean,name)
-                
-
-
-
-
-
-
-
+    # Notice, I do not reset the name as the user has elected to play again
+    start(nice, mean, name)
 
 if __name__ == "__main__":
     start()
